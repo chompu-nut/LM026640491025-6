80cdd1661b07fb3b342159bafb8ce697a9faeab8
chompu-nut <chompunut.net@rmutto.ac.th>
2024-07-11 16:08:21 +0700

    Week02--Web Programming --cards import export

---
+++ app/routes/Data.tsx
@@ -0,0 +1,32 @@
+export const Cards = [
+    {
+        id:100,
+        name: "Duanghathai",
+        biog: "นักเรียน",
+        bgProf: "/images/3.png",
+        userIcon: "/images/4.jpg",
+        userName: "ตอง",
+        creadAt: "tuy 10,2024",
+        active:false
+    },
+    {
+        id:102,
+        name: "choompunut ",
+        biog: "นักเรียน",
+        bgProf: "/images/1.jpg",
+        userIcon: "/images/4.jpg",
+        userName: "chompu",
+        creadAt: "tuy 10,2024",
+        active: true
+    },
+    {
+        id:103,
+        name: "June",
+        biog: "นักเรียน",
+        bgProf: "/images/2.png",
+        userIcon: "/images/4.jpg",
+        userName: "จูน",
+        creadAt: "tuy 10,2024",
+        active: true
+    },
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,57 +1,60 @@
 import type { MetaFunction } from "@remix-run/node";
+import MyCads from "./MyCards";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "choompunut Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <hr/>
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>Name : chompunut nevisit</li>
-        <li>class : Information Technology</li>
-        <li>E-mail :< a href ="mailto : chompunut.net@rmutto.ac.th">Contact me.</a> </li>
-        <li><img src=" https://www.beartai.com/wp-content/uploads/2018/09/EDBBD0D7-372A-429A-85F5-8F316B3551B6.jpeg" alt="" />
-        </li>
-        </ul>
-    </div>
+    <MyCads />
+
+    // <div className="font-sans p-4">
+    //   <h1 className="text-3xl">Welcome to Remix</h1>
+    //   <ul className="list-disc mt-4 pl-6 space-y-2">
+    //     <li>
+    //       <a
+    //         className="text-blue-700 underline visited:text-purple-900"
+    //         target="_blank"
+    //         href="https://remix.run/start/quickstart"
+    //         rel="noreferrer"
+    //       >
+    //         5m Quick Start
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         className="text-blue-700 underline visited:text-purple-900"
+    //         target="_blank"
+    //         href="https://remix.run/start/tutorial"
+    //         rel="noreferrer"
+    //       >
+    //         30m Tutorial
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         className="text-blue-700 underline visited:text-purple-900"
+    //         target="_blank"
+    //         href="https://remix.run/docs"
+    //         rel="noreferrer"
+    //       >
+    //         Remix Docs
+    //       </a>
+    //     </li>
+    //   </ul>
+    //   <hr/>
+    //   <h1 className="text-3xl">Welcome to Remix</h1>
+    //   <ul className="list-disc mt-4 pl-6 space-y-2">
+    //     <li>Name : chompunut nevisit</li>
+    //     <li>class : Information Technology</li>
+    //     <li>E-mail :< a href ="mailto : chompunut.net@rmutto.ac.th">Contact me.</a> </li>
+    //     <li><img src=" https://www.beartai.com/wp-content/uploads/2018/09/EDBBD0D7-372A-429A-85F5-8F316B3551B6.jpeg" alt="" />
+    //     </li>
+    //     </ul>
+    // </div>
   );
 }
